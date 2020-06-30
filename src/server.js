19e32c31bbad627ab0d047c5cc98634e6b89acf3
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-06-30 20:37:55 +0200

    second

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log(process.env.PORT)
+const express = require("express") //crea la funzione
+const server = express()//invoca la funzione express
+
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`Server run in port ${port}`)
+}) 
